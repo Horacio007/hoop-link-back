bdd4f1360abc6e5378d86fe8b2e73c417f2d25f9
DesarrolloPLS <desarrollo@plasalle.mx>
2025-08-03 13:30:11 -0600

    otros cambios

--- src/main.ts
+++ src/main.ts
@@ -43,7 +43,7 @@ async function bootstrap() {
 
   app.setGlobalPrefix('api');
 
-  await app.listen(process.env.PORT_LAUNCH);
+  await app.listen(process.env.PORT_LAUNCH || '0.0.0.0');
   logger.log(`App running on port ${process.env.PORT_LAUNCH}`);
 }
 bootstrap();
