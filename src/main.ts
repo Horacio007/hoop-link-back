2cb4b567db7a8fa7750cf7cc937cc9bf413f3425
DesarrolloPLS <desarrollo@plasalle.mx>
2025-09-04 16:02:31 -0600

    cabm,ios

--- src/main.ts
+++ src/main.ts
@@ -43,7 +43,8 @@ async function bootstrap() {
 
   app.setGlobalPrefix('api');
 
-  await app.listen(process.env.PORT_LAUNCH || 3000, '0.0.0.0');
+  await app.listen(process.env.PORT || 3000, '0.0.0.0');
   logger.log(`App running on port ${process.env.PORT_LAUNCH}`);
+  logger.log(`App running on port ${process.env.PORT}`);
 }
 bootstrap();
